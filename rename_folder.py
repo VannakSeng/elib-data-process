755bc1546c5eba054ef67463a35ff128c55c8f03
Phanith LIM <limphanith.office@gmail.com>
2023-11-01 09:59:45 +0700

    watermark 1.7

--- rename_folder.py
+++ rename_folder.py
@@ -1,10 +1,16 @@
 import os
 
 root = "data"
-for folder in os.listdir(root):
-    if not folder.startswith('.'):
-        new_name = f"{root} {folder}"
-        old_folder_path = os.path.join(root, folder)
-        new_folder_path = os.path.join(root, new_name)
+folders = [folder for folder in os.listdir(root) if not folder.startswith('.')]
+folders.remove("output")
+print(folders)
+for sub_folder in folders:
+    path = os.path.join(root, sub_folder)
+    folder = [folder for folder in os.listdir(path) if not folder.startswith('.')]
+    for change in folder:
+        new_name = f"{sub_folder} {change}"
+        sub_path = os.path.join(root, sub_folder)
+        old_folder_path = os.path.join(sub_path, change)
+        new_folder_path = os.path.join(sub_path, new_name)
         os.rename(old_folder_path, new_folder_path)
 
